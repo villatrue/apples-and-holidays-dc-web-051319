18b8d184e2263baaffdfc27b1c9329e164ccf094
Sean Carter <scarter0313@gmail.com>
2019-05-12 02:59:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
  holiday_hash.map do |season, holidays|
    holidays.map do |season, supplies|
-     holiday if supplies.include? 
+     holiday if supplies.include?("BBQ")
+   end
+ end
+ 
 
 end
 
