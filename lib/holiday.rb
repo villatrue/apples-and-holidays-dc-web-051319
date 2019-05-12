59c9ae690e6eb1f0cb0d748118300b10a33003d9
Sean Carter <scarter0313@gmail.com>
2019-05-12 02:31:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,6 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
  holiday_hash[:winter].each do |holiday, supplies|
    supplies<< supply
 end
+end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
