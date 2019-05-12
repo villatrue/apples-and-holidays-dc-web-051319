ceadfb172354c6722114c50c6a8fef2c34fe12f7
Sean Carter <scarter0313@gmail.com>
2019-05-12 02:28:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,7 +19,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # }
   # return the second element in the 4th of July array
   holiday_hash[:winter].each do |holiday,supply|
-    
+    su
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
